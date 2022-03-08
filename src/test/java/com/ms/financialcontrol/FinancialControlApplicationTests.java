0dd5881d7656d7638f07ec8e5909e481bdaa5244
Pedro Nandi <pedro.nandi@matera.com>
2022-03-08 16:49:36 -0300

    FinancialControl: Basic project structure

---
+++ src/test/java/com/ms/financialcontrol/FinancialControlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ms.financialcontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancialControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
